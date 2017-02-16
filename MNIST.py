c0ae2e73af8a772b642a51aeb0df5ca56131ccd0
njfritter <nathan.fritter@yahoo.com>
2017-02-15 19:11:35 -0800

    Added file for official breast cancer detection

--- MNIST.py
+++ MNIST.py
@@ -162,119 +162,27 @@ def neuralNetwork():
 #######
 
 def kNearestNeighbors():
+	# Will potentially build from scratch in the future from below links
 	# https://lazyprogrammer.me/tutorial-k-nearest-neighbor-classifier-for-mnist/
 	# http://andrew.gibiansky.com/blog/machine-learning/k-nearest-neighbors-simplest-machine-learning/
 	import random
-	def predict(self, point):
-    # We have to copy the data set list, because once we've located the best
-    # candidate from it, we don't want to see that candidate again, so we'll delete it.
-	    candidates = self.dataset[:]
-	    
-	    # Loop until we've gotten all the neighbors we want.
-	    neighbors = []
-	    while len(neighbors) < self.k:
-	        # Compute distances to every candidate.
-	        distances = [self.distance(x[0], point) for x in candidates]
-	        
-	        # Find the minimum distance neighbor.
-	        best_distance = min(distances)
-	        index = distances.index(best_distance)
-	        neighbors.append(candidates[index])
-	        
-	        # Remove the neighbor from the candidates list.
-	        del candidates[index]
-	    
-	    # Predict by averaging the closets k elements.
-	    prediction = self.consensus([value[1] for value in neighbors])
-	    return prediction
+	from sklearn.neighbors import KNeighborsClassifier
 
-	# Get the figure and axes.
-	fig, axes = plt.subplots(5, 5)
-	axes = axes.reshape(25)
-	fig.suptitle("Random Sampling of MNIST")
-
-	# Plot random images.
-	indices = random.randint(len(train_images), size=25)
-	for axis, index in zip(axes, indices):
-	    image = train_images[index, :, :]
-	    axis.get_xaxis().set_visible(False)
-	    axis.get_yaxis().set_visible(False)
-	    axis.imshow(image, cmap = cm.Greys_r)
-
-	# Let's go ahead and replace the old implementation.
-	NearestNeighborClassifier.predict = predict
-
-	def euclidean_distance(img1, img2):
-	    # Since we're using NumPy arrays, all our operations are automatically vectorized.
-	    # A breakdown of this expression:
-	    #     img1 - img2 is the pixel-wise difference between the images
-	    #     (img1 - img2) ** 2 is the same thing, with each value squared
-	    #     sum((img1 - img2) ** 2) is the sum of the elements in the matrix.
-	    return sum((img1 - img2) ** 2)
-
-	from collections import defaultdict
-	def get_majority(votes):
-	    # For convenience, we're going to use a defaultdict.
-	    # This is just a dictionary where values are initialized to zero
-	    # if they don't exist.
-	    counter = defaultdict(int)
-	    for vote in votes:
-	        # If this weren't a defaultdict, this would error on new vote values.
-	        counter[vote] += 1
-	    
-	    # Find out who was the majority.
-	    majority_count = max(counter.values())
-	    for key, value in counter.items():
-	        if value == majority_count:
-	            return key
-
-	# Create the predictor class.
-	class MNISTPredictor(NearestNeighborClassifier):
-	    def distance(self, p1, p2):
-	        return euclidean_distance(p1, p2)
-	    
-	    def consensus(self, values):
-	        return get_majority(values)
-	    
-	# Convert our data set into an easy format to use.
-	# This is a list of (x, y) pairs. x is an image, y is a label.
-	dataset = []
-	for i in xrange(len(train_images)):
-	    dataset.append((train_images[i, :, :], train_labels[i]))
-	    
-	# Create a predictor for various values of k.
-	ks = [1, 2, 3, 4, 5, 6]
-	predictors = [MNISTPredictor(dataset, k) for k in ks] 
+	X, y = mnist.data / 255., mnist.target
+	X_train, X_test = X[:60000], X[60000:]
+	y_train, y_test = y[:60000], y[60000:]
 
 
-	def predict_test_set(predictor, test_set):
-	    """Compute the prediction for every element of the test set."""
-	    predictions = [predictor.predict(test_set[i, :, :]) 
-	                   for i in xrange(len(test_set))]
-	    return predictions
+	KNN = KNeighborsClassifier(n_neighbors=5)
+	KNN.fit(X_train, y_train)
 
-	# Choose a subset of the test set. Otherwise this will never finish.
-	test_set = test_images[0:100, :, :]
-	all_predictions = [predict_test_set(predictor, test_set) for predictor in predictors]
+	KNN_pred = KNN.predict(X_test)
+	
+	# Cross Validation Results Exercise 3.3 for Decision Tree
+	scores = cross_validation.cross_val_score(clf, X_train, y_train.tolist(), cv = 5)
+	print(scores)
+	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2))
 
-	def evaluate_prediction(predictions, answers):
-	    """Compute how many were identical in the answers and predictions,
-	    and divide this by the number of predictions to get a percentage."""
-	    correct = sum(asarray(predictions) == asarray(answers))
-	    total = float(prod(answers.shape))
-	    return correct / total
-
-	labels = asarray(test_labels[0:100])
-	accuracies = [evaluate_prediction(pred, labels) for pred in all_predictions]
-
-	# Draw the figure.
-	fig = figure(1)
-	plt.plot(ks, accuracies, 'ro', figure=fig)
-
-	fig.suptitle("Nearest Neighbor Classifier Accuracies")
-	fig.axes[0].set_xlabel("k (# of neighbors considered)")
-	fig.axes[0].set_ylabel("accuracy (% correct)");
-	fig.axes[0].axis([0, max(ks) + 1, 0, 1]);
 
 
 def decisionTree():
@@ -300,7 +208,7 @@ def decisionTree():
 
 	predictionRes = clf.predict(X_test)
 
-	print(metrics.classification_report(y_test.tolist(), predictionRes, digits = 4))
+	
 
 	# Cross Validation Results Exercise 3.3 for Decision Tree
 	scores = cross_validation.cross_val_score(clf, X_train, y_train.tolist(), cv = 5)
